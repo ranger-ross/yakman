b7aea95dc11e247db1ec95238ef6661f1c4daea1
Ross <ross.sullivan101@gmail.com>
2023-03-21 21:53:03 +0900

    Added some basic structs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
+use rocket::http::Status;
+use rocket::response::{content, status};
+use rocket::serde::json::Json;
+use serde::Serialize;
+
 #[macro_use]
 extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[get("/config")]
+fn index() -> Json<AppConfig> {
+    return Json(AppConfig {
+        id: 123,
+        name: "test".to_string(),
+    });
+}
+
+#[derive(Serialize)]
+struct AppConfig {
+    id: i32,
+    name: String,
+}
+
+#[derive(Serialize)]
+struct AppLabelType {
+    id: i32,
+    name: String,
+}
+
+#[derive(Serialize)]
+struct AppLabel {
+    label_type: AppLabelType,
+    value: String, // TODO: more powerful generics?
+}
+
+#[derive(Serialize)]
+struct AppConfigInstance {
+    config: AppConfig,
+    content: String,
+    labels: Vec<AppLabel>,
+}
+
+fn load_config() -> AppConfigInstance {
+    return AppConfigInstance {
+        config: AppConfig {
+            id: 100,
+            name: "FirstConfig".to_string(),
+        },
+        content: "this is my config data".to_string(),
+        labels: vec![],
+    };
 }
