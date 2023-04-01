6a88a5fad385ddf386845f06023d386fea636f4c
Ross <ross.sullivan101@gmail.com>
2023-04-01 12:05:41 +0900

    more refactoring

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,16 @@ mod adapters;
 mod config_man;
 mod config_man_state;
 mod data_types;
-mod raw_query;
+mod utils;
 
-use adapters::ConfigStorageAdapter;
-use data_types::{Config, ConfigInstance, Label, LabelType};
 use adapters::local_file_adapter::LocalFileStorageAdapter;
 use adapters::postgres_adapter::PostgresAdapter;
 use adapters::redis_adapter::RedisStorageAdapter;
+use adapters::ConfigStorageAdapter;
+use data_types::{Config, ConfigInstance, Label, LabelType};
 use rocket::{serde::json::Json, State};
 use std::vec;
-
-use raw_query::RawQuery;
+use utils::raw_query::RawQuery;
 
 #[macro_use]
 extern crate rocket;
@@ -29,7 +28,6 @@ impl StateManager {
 
 #[launch]
 async fn rocket() -> _ {
-
     let settings = config_man::load_config_man_settings();
     println!("Settings: {:?}", settings);
 
@@ -79,7 +77,10 @@ async fn data(config_name: &str, query: RawQuery, state: &State<StateManager>) -
         })
         .collect();
 
-    println!("Search for config {} with labels: {:?}", config_name, labels);
+    println!(
+        "Search for config {} with labels: {:?}",
+        config_name, labels
+    );
 
     return adapter.get_config_data(config_name, labels).await;
 }
@@ -98,12 +99,11 @@ fn get_redis_adapter() -> impl ConfigStorageAdapter {
         password: "".to_string(),
     };
 }
- fn get_postgres_adapter() -> impl ConfigStorageAdapter {
+fn get_postgres_adapter() -> impl ConfigStorageAdapter {
     return PostgresAdapter {
         host: "localhost".to_string(),
         port: 5432,
         username: "postgres".to_string(),
-        password: "password".to_string()
-
+        password: "password".to_string(),
     };
- }
+}
