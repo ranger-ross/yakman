6b4562cceff79e809fcaccbd92e7e0b12bbde973
Ross <ross.sullivan101@gmail.com>
2023-04-07 21:20:48 +0900

    Added ability to load all config instances

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,47 +3,70 @@ use gloo_net::http::Request;
 use yak_man_core::model::{Config, ConfigInstance, LabelType};
 use yew::prelude::*;
 
+#[derive(Debug, PartialEq)]
+struct PageConfig {
+    config: Config,
+    instances: Vec<ConfigInstance>,
+}
+
+#[derive(Debug, PartialEq)]
+struct PageData {
+    configs: Vec<PageConfig>,
+    labels: Vec<LabelType>,
+}
+
 #[function_component(App)]
 fn app() -> Html {
-    let configs = use_state(|| vec![]);
-    let labels = use_state(|| vec![]);
-    let first_instances = use_state(|| vec![]);
+    let page_data: UseStateHandle<Option<PageData>> = use_state(|| None);
 
     {
-        let configs = configs.clone();
-        let first_instances = first_instances.clone();
+        let page_data = page_data.clone();
         use_effect_with_deps(
             move |_| {
                 wasm_bindgen_futures::spawn_local(async move {
-                    let fetched_configs: Vec<Config> = fetch_configs().await;
+                    let mut configs_list = vec![];
 
-                    let first_config_name = &fetched_configs[0].name.clone();
-                    configs.set(fetched_configs);
+                    for config in fetch_configs().await {
+                        let instances = fetch_instance_metadata(&config.name).await;
+                        configs_list.push(PageConfig {
+                            config: config,
+                            instances: instances,
+                        });
+                    }
 
-                    log!("Hello -> ", first_config_name);
-                    let f = fetch_instance_metadata(&first_config_name).await;
-                    first_instances.set(f);
+                    let labels: Vec<LabelType> = fetch_labels().await;
 
-                    let fetched_labels: Vec<LabelType> = fetch_labels().await;
-                    labels.set(fetched_labels);
+                    page_data.set(Some(PageData {
+                        configs: configs_list,
+                        labels: labels,
+                    }));
                 });
             },
             (),
         );
     }
 
+    if page_data.is_none() {
+        return html! {
+            <div> {"Loading..."} </div>
+        };
+    }
+
     html! {
       <>
         <h1>{ "Configs" }</h1>
 
-        {configs.iter().map(|video| {
+        {page_data.as_ref().unwrap().configs.iter().map(|config| {
             html! {
                 <>
-                    <h3 key={video.name.clone()}>{format!("{}: {}", video.name, video.description)}</h3>
-                    {first_instances.iter().map(|instance| {
-                        html! { <ConfigInstanceRow key={instance.instance.clone()} instance={instance.clone()} /> } // TODO: fetch data dynamically
+                    <h2>{&config.config.name}</h2>
+
+                    {config.instances.iter().map(|instance| {
+                        html! {
+                            <ConfigInstanceRow key={instance.instance.clone()} instance={instance.clone()} />
+                        }
                     }).collect::<Html>()}
-            
+
                 </>
             }
         }).collect::<Html>()}
