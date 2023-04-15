373d95ffc365faad92f5c9e5a7516197e9128acf
Ross <ross.sullivan101@gmail.com>
2023-04-15 13:32:30 +0900

    Clean up

--- backend/src/adapters/errors.rs
+++ backend/src/adapters/errors.rs
@@ -1,35 +1,22 @@
-use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum CreateConfigError {
+    #[error("Duplicate config: `{name}`")]
     DuplicateConfigError { name: String },
+    #[error("Error storing config: {message}")]
     StorageError { message: String },
-}
-
-impl std::error::Error for CreateConfigError {}
-
-impl fmt::Display for CreateConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            CreateConfigError::DuplicateConfigError { name } => {
-                write!(f, "Duplicate config: `{name}`")
-            }
-            CreateConfigError::StorageError { message } => {
-                write!(f, "Error storing config: {message}")
-            }
-        }
-    }
 }
 
 impl CreateConfigError {
     pub fn duplicate_config_error(name: &str) -> CreateConfigError {
-        return CreateConfigError::DuplicateConfigError {
+        CreateConfigError::DuplicateConfigError {
             name: String::from(name),
-        };
+        }
     }
     pub fn storage_error(message: &str) -> CreateConfigError {
-        return CreateConfigError::StorageError {
+        CreateConfigError::StorageError {
             message: String::from(message),
-        };
+        }
     }
 }
