d9ff327e6c9ae99030531d6e12d685414bde45f3
ranger-ross <ross.sullivan101@gmail.com>
2023-04-25 20:45:47 +0900

    local file adapter clean up

---
+++ backend/src/adapters/local_file_adapter/storage_types.rs
@@ -0,0 +1,22 @@
+use serde::{Serialize, Deserialize};
+use yak_man_core::model::{LabelType, Config, ConfigInstance, ConfigInstanceRevision};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LabelJson {
+    pub labels: Vec<LabelType>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ConfigJson {
+    pub configs: Vec<Config>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InstanceJson {
+    pub instances: Vec<ConfigInstance>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RevisionJson {
+    pub revision: ConfigInstanceRevision,
+}
