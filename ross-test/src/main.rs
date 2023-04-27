3175f44345612d1b8c07385001e86e9700186fcc
ranger-ross <ross.sullivan101@gmail.com>
2023-04-27 23:32:33 +0900

    basic leptos app

---
+++ ross-test/src/main.rs
@@ -0,0 +1,37 @@
+use leptos::*;
+
+fn main() {
+    mount_to_body(|cx| view! { cx, 
+        <div>
+            <p>"Hello, world!"</p>
+            <SimpleCounter 
+                initial_value=0
+                step=1
+            />
+        </div>
+    })
+}
+
+
+/// A simple counter component.
+/// 
+/// You can use doc comments like this to document your component.
+#[component]
+pub fn SimpleCounter(
+    cx: Scope,
+    /// The starting value for the counter
+    initial_value: i32,
+    /// The change that should be applied each time the button is clicked.
+    step: i32
+) -> impl IntoView {
+    let (value, set_value) = create_signal(cx, initial_value);
+
+    view! { cx,
+        <div>
+            <button on:click=move |_| set_value(0)>"Clear"</button>
+            <button on:click=move |_| set_value.update(|value| *value -= step)>"-1"</button>
+            <span>"Value: " {value} "!"</span>
+            <button on:click=move |_| set_value.update(|value| *value += step)>"+1"</button>
+        </div>
+    }
+}
