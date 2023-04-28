87f0adbae60d501e46d232737a49821f979744e6
ranger-ross <ross.sullivan101@gmail.com>
2023-04-28 19:46:31 +0900

    Ported main page to leptos

--- ross-test/src/main.rs
+++ ross-test/src/main.rs
@@ -1,16 +1,18 @@
 use leptos::*;
 
-use crate::{add_label_page::AddLabelPage, add_config_page::AddConfigPage};
+use crate::{add_label_page::AddLabelPage, add_config_page::AddConfigPage, config_list_page::ConfigListPage};
 
 mod add_label_page;
 mod api;
 mod add_config_page;
+mod config_list_page;
 
 fn main() {
     mount_to_body(|cx| {
         view! { cx,
             <div>
-                <AddConfigPage />
+                <ConfigListPage />
+                // <AddConfigPage />
                 // <AddLabelPage />
             </div>
         }
