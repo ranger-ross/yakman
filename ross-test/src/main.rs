792dfff757b8fc2209def9c8b868a1e29644a733
ranger-ross <ross.sullivan101@gmail.com>
2023-04-28 20:59:07 +0900

    Setup leptos router

--- ross-test/src/main.rs
+++ ross-test/src/main.rs
@@ -1,20 +1,41 @@
-use leptos::*;
-
-use crate::{add_label_page::AddLabelPage, add_config_page::AddConfigPage, config_list_page::ConfigListPage};
-
+mod add_config_page;
 mod add_label_page;
 mod api;
-mod add_config_page;
 mod config_list_page;
 
-fn main() {
-    mount_to_body(|cx| {
-        view! { cx,
-            <div>
-                <ConfigListPage />
-                // <AddConfigPage />
-                // <AddLabelPage />
-            </div>
-        }
-    })
+use add_config_page::*;
+use add_label_page::*;
+use config_list_page::*;
+use leptos::*;
+use leptos_router::*;
+
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|cx| view! { cx, <RouterExample/> })
 }
+
+#[component]
+pub fn RouterExample(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <Router>
+            <main>
+                <Routes>
+                    <Route
+                        path="/"
+                        view=move |cx| view! { cx,  <ConfigListPage /> }
+                    />
+                    <Route
+                        path="add-config"
+                        view=move |cx| view! { cx,  <AddConfigPage /> }
+                    />
+                    <Route
+                        path="add-label"
+                        view=move |cx| view! { cx,  <AddLabelPage /> }
+                    />
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
