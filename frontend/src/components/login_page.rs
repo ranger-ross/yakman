7a160895aa39ff93af28db9ffd6630213abedd05
Ross <rosssullivan101@gmail.com>
2023-05-09 21:47:34 +0900

    Added some very basic css improvements to make the website less ugly (#48)

--- frontend/src/components/login_page.rs
+++ frontend/src/components/login_page.rs
@@ -6,7 +6,6 @@ use gloo_storage::Storage;
 use leptos::*;
 use leptos_router::*;
 use oauth2::{PkceCodeChallenge, PkceCodeVerifier};
-use wasm_bindgen_futures::spawn_local;
 
 const LOCAL_STORAGE_OAUTH2_VERIFER_KEY: &str = "oauth2-verifier";
 
@@ -30,20 +29,13 @@ pub fn login_page(cx: Scope) -> impl IntoView {
 
     let redirect_uri = move || data.read(cx).unwrap_or(String::new());
 
-    let on_refresh_token = move |_| {
-        spawn_local(async {
-            api::refresh_token().await.unwrap();
-        })
-    };
-
     view! { cx,
-        <div>
-            <h1>{"Login"}</h1>
+        <div style="display: flex; justify-content: center">
+            <div>
+                <h1>{"Login"}</h1>
 
-            <a href=redirect_uri>"Click to login"</a>
-
-            <br />
-            <button on:click=on_refresh_token>"Refresh Token"</button>
+                <a href=redirect_uri>"Click to login"</a>
+            </div>
         </div>
     }
 }
@@ -98,22 +90,22 @@ pub fn oauth_callback_page(cx: Scope) -> impl IntoView {
     let error_message = move || error_message.read(cx).unwrap_or(None);
 
     view! { cx,
-        <div>
-
-            {move || match error_message() {
-                Some(error) => view! { cx,
-                    <>
-                        {error} <br />
-                        <A href="/login">
-                            "Back to Login"
-                        </A>
-                    </>
-                }.into_view(cx),
-                None => view! {cx,
-                    <h1>{"Logging in..."}</h1>
-                }.into_view(cx)
-            }}
-
+        <div style="display: flex; justify-content: center">
+            <div>
+                {move || match error_message() {
+                    Some(error) => view! { cx,
+                        <>
+                            {error} <br />
+                            <A href="/login">
+                                "Back to Login"
+                            </A>
+                        </>
+                    }.into_view(cx),
+                    None => view! {cx,
+                        <h1>{"Logging in..."}</h1>
+                    }.into_view(cx)
+                }}
+            </div>
         </div>
     }
 }
