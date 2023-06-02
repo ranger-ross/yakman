e6b39dc18660c4fed41086fba65343c854ca676e
ranger-ross <rosssullivan101@gmail.com>
2023-06-02 20:33:09 +0900

    bug fix

--- frontend/src/components/select.rs
+++ frontend/src/components/select.rs
@@ -9,11 +9,12 @@ pub fn yak_man_select(
     #[prop(into)] label: MaybeSignal<Cow<'static, str>>,
     children: Children,
 ) -> impl IntoView {
+    let name = label.clone();
     view! { cx,
         <div class="w-64">
             <label class="block text-gray-700 text-sm font-bold mb-2">{label}</label>
             <div class="relative">
-                <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                <select name={name} class="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                     {children(cx)}
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
