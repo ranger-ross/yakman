0964265929360a4bd011b7c6ba750841349a9c97
ranger-ross <rosssullivan101@gmail.com>
2023-06-03 13:05:55 +0900

    Animtation improvements

--- frontend/src/components/input.rs
+++ frontend/src/components/input.rs
@@ -13,7 +13,7 @@ pub fn yak_man_input(
             <div class="relative">
                 <input
                     type="text"
-                    class="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                    class="block appearance-none w-full bg-white border border-gray-400 hover:border-indigo-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline transition-all duration-200"
                     placeholder={placeholder}
                     prop:value=value
                 />
