cc07b2a97166f9cee210736830ec63e2c7aadc24
Ross <rosssullivan101@gmail.com>
2023-06-03 13:11:11 +0900

    Improved component CSS animations  (#73)

--- frontend/src/components/button.rs
+++ frontend/src/components/button.rs
@@ -8,6 +8,6 @@ pub fn yak_man_button(
 ) -> impl IntoView {
     let disabled = disabled.unwrap_or(false);
     view! { cx,
-        <button disabled={disabled} class="bg-indigo-600 disabled:bg-gray-300 text-white text-lg font-bold rounded-lg shadow-md py-1 px-4 m-1">{children(cx)}</button>
+        <button disabled={disabled} class="bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-300 text-white text-lg font-bold rounded-lg shadow-md py-1 px-4 m-1 transition-colors duration-200">{children(cx)}</button>
     }
 }
