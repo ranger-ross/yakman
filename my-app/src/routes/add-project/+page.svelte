91a16aa1a7be7b063e73a9f382a39f087e6a448a
ranger-ross <rosssullivan101@gmail.com>
2023-07-22 14:57:13 +0900

    Added basic add project page

--- my-app/src/routes/add-project/+page.svelte
+++ my-app/src/routes/add-project/+page.svelte
@@ -1,3 +1,36 @@
-<div>
-    add project
+<script lang="ts">
+    import YakManButton from "$lib/components/YakManButton.svelte";
+    import YakManCard from "$lib/components/YakManCard.svelte";
+    import YakManInput from "$lib/components/YakManInput.svelte";
+
+    let name = "";
+
+    function onCreateProject() {
+        console.log("on create", name);
+
+        // TODO: FIX
+
+        // match api::create_project(&name).await {
+        //         Ok(()) => {
+        //             let navigate = use_navigate(cx);
+        //             let _ = navigate("/", Default::default());
+        //         }
+        //         Err(err) => error!("Error creating project: {}", err.to_string()),
+        //     };
+    }
+</script>
+
+<div class="container mx-auto">
+    <YakManCard>
+        <h1 class="text-lg font-bold mb-4">{"Add Project"}</h1>
+        <div class="mb-3">
+            <!-- TODO: Handle text masking -->
+            <YakManInput
+                label="Name"
+                placeholder="my-project"
+                bind:value={name}
+            />
+        </div>
+        <YakManButton on:click={onCreateProject}>Create</YakManButton>
+    </YakManCard>
 </div>
