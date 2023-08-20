a54396dca151c214cff1607d037cfad7793d3dc5
ranger-ross <rosssullivan101@gmail.com>
2023-08-20 12:45:19 +0900

    Added approved confirm model

--- frontend/src/routes/apply-changes/[config]/[instance]/+page.svelte
+++ frontend/src/routes/apply-changes/[config]/[instance]/+page.svelte
@@ -3,6 +3,7 @@
     import { page } from "$app/stores";
     import YakManButton from "$lib/components/YakManButton.svelte";
     import YakManCard from "$lib/components/YakManCard.svelte";
+    import { openGlobaModal } from "$lib/stores/global-modal-state";
     import { trpc } from "$lib/trpc/client";
     import type { PageData } from "./$types";
 
@@ -10,7 +11,17 @@
 
     let { config, instance } = $page.params;
 
-    async function onApprove() {
+    function onApprove() {
+        openGlobaModal({
+            title: "Approve Changes",
+            message: "Are you sure you want to approve this change? It will be applied immediately",
+            onConfirm() {
+                saveChanges();
+            },
+        });
+    }
+
+    async function saveChanges() {
         try {
             await trpc($page).revisions.approveInstanceRevision.mutate({
                 configName: config,
