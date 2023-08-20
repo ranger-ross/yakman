06f237854c5c2ff7e4df86cae6232a8d056b093f
ranger-ross <rosssullivan101@gmail.com>
2023-08-20 15:18:45 +0900

    Added logic to disable update button in modify instance page

--- frontend/src/routes/modify-instance/[config]/[[instance]]/+page.svelte
+++ frontend/src/routes/modify-instance/[config]/[[instance]]/+page.svelte
@@ -16,6 +16,9 @@
     export let data: PageData;
     let { labels } = data;
     let selectedLabels: { [labelName: string]: string } = data.selectedLabels; // <LabelName, Value>
+    let originalSelectedLabels = JSON.parse(
+        JSON.stringify(data.selectedLabels)
+    ); // Create a clone of the data
 
     let input = data.data?.data ?? "";
     let contentType = data.data?.contentType ?? "text/plain";
@@ -66,6 +69,32 @@
             console.error(e);
         }
     }
+
+    let hasChanges = false;
+    $: {
+        hasChanges = (() => {
+            if (input !== data.data?.data) {
+                return true;
+            }
+            if (contentType !== data.data?.contentType) {
+                return true;
+            }
+
+            if (
+                Object.keys(originalSelectedLabels).length !==
+                Object.keys(selectedLabels).length
+            ) {
+                return true;
+            }
+
+            for (const key of Object.keys(originalSelectedLabels)) {
+                if (originalSelectedLabels[key] !== selectedLabels[key]) {
+                    return true;
+                }
+            }
+            return false;
+        })();
+    }
 </script>
 
 <div class="container mx-auto">
@@ -90,7 +119,7 @@
             />
         </div>
         <LabelSelection {labels} bind:selectedLabels />
-        <YakManButton on:click={onSubmit}>
+        <YakManButton on:click={onSubmit} disabled={!hasChanges}>
             {#if editMode}
                 Update
             {:else}
