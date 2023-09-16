2ca5b7487bab7601c3a1860efd41ead79fae15bc
ranger-ross <rosssullivan101@gmail.com>
2023-09-16 16:43:43 +0900

    Enabled yaml in the editor

--- frontend/src/lib/utils/content-type-utils.ts
+++ frontend/src/lib/utils/content-type-utils.ts
@@ -1,9 +1,12 @@
-export type MonacoLanguage = "json" | "html" | "typescript" | "css" | "text";
+export type MonacoLanguage = "json" | "html" | "typescript" | "css" | "text" | "yaml";
 
 
 export function contentTypeToMonacoLanguage(contentType?: string | null): MonacoLanguage {
     if (!contentType)
         return "text";
+    if (contentType.includes("yaml") || contentType.includes("yml")) {
+        return "yaml";
+    }
     switch (contentType) {
         case "application/json":
             return "json";
