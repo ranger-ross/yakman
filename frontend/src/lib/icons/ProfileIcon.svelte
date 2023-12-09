8e6194e478be19faa0caab8b2b5cfc6a138b23d8
ranger-ross <rosssullivan101@gmail.com>
2023-12-09 11:12:28 +0900

    contrast

--- frontend/src/lib/icons/ProfileIcon.svelte
+++ frontend/src/lib/icons/ProfileIcon.svelte
@@ -2,7 +2,7 @@
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    class="h-10 w-10"
+    class="h-10 w-10 contrast-0"
     ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
         id="SVGRepo_tracerCarrier"
         stroke-linecap="round"
