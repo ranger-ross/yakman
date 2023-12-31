6cd72bb33146957edbbd9c331c962933318ca2f7
ranger-ross <rosssullivan101@gmail.com>
2023-12-31 10:37:59 +0900

    code cleanup

--- frontend/src/routes/add-config/+page.svelte
+++ frontend/src/routes/add-config/+page.svelte
@@ -12,11 +12,10 @@
 
     let name = "";
     let defaultProjectUuid = $page.url.searchParams.get("project");
-    let selectedProjectUuid =
-        !!defaultProjectUuid &&
-        data.projects.some((p) => p.uuid === defaultProjectUuid)
-            ? defaultProjectUuid
-            : data.projects[0].uuid;
+    let defaultProject =
+        data.projects.find((p) => p.uuid === defaultProjectUuid) ??
+        data.projects[0];
+    let selectedProjectUuid = defaultProject.uuid;
 
     async function onCreateConfig() {
         try {
