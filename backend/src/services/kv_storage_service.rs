600c41c969ed4cfa291109bb20d8cbc8537b6e43
Ross <rosssullivan101@gmail.com>
2024-01-15 20:32:41 +0900

    Updated generate revision id function (#270)

--- backend/src/services/kv_storage_service.rs
+++ backend/src/services/kv_storage_service.rs
@@ -116,8 +116,8 @@ impl StorageService for KVStorageService {
         creator_uuid: &str,
     ) -> Result<String, CreateConfigInstanceError> {
         if let Some(mut instances) = self.adapter.get_instance_metadata(config_name).await? {
-            let instance = short_sha(&Uuid::new_v4().to_string());
-            let revision_key: String = short_sha(&Uuid::new_v4().to_string());
+            let instance = generate_instance_id();
+            let revision_key: String = generate_revision_id();
             let data_key = Uuid::new_v4().to_string();
             let now = Utc::now().timestamp_millis();
 
@@ -318,7 +318,7 @@ impl StorageService for KVStorageService {
             return Err(SaveConfigInstanceError::InvalidLabel);
         }
 
-        let revision_key = short_sha(&Uuid::new_v4().to_string());
+        let revision_key = generate_revision_id();
         let data_key = Uuid::new_v4().to_string();
 
         // Create new file with data
@@ -575,7 +575,7 @@ impl StorageService for KVStorageService {
             .await?
             .ok_or(RollbackRevisionError::InvalidRevision)?;
 
-        let revision_key = short_sha(&Uuid::new_v4().to_string());
+        let revision_key = generate_revision_id();
 
         // Create revision
         let now = Utc::now().timestamp_millis();
@@ -787,8 +787,56 @@ impl KVStorageService {
     }
 }
 
+fn generate_instance_id() -> String {
+    return format!("i{}", short_sha(&Uuid::new_v4().to_string()));
+}
+
+fn generate_revision_id() -> String {
+    return format!("r{}", short_sha(&Uuid::new_v4().to_string()));
+}
+
 /// Returns a 12 character string representation of a SHA256
 fn short_sha(input: &str) -> String {
     let sha: String = sha256::digest(input);
     return sha[0..12].to_string();
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_short_sha() {
+        let result = short_sha("hello world");
+        assert_eq!(result, "b94d27b9934d");
+
+        let result = short_sha("foo");
+        assert_eq!(result, "2c26b46b68ff");
+
+        let result = short_sha("bar");
+        assert_eq!(result, "fcde2b2edba5");
+
+        let result = short_sha("ade10004-41df-4bf6-88b9-d768afab674f");
+        assert_eq!(result, "8146205a8d27");
+    }
+
+    #[test]
+    fn test_generate_instance_id() {
+        for _i in 0..10 {
+            let result = generate_instance_id();
+            assert_eq!(13, result.len());
+            assert!(result.starts_with('i'));
+        }
+    }
+
+    #[test]
+    fn test_generate_revision_id() {
+        for _i in 0..10 {
+            let result = generate_revision_id();
+            assert_eq!(13, result.len());
+            assert!(result.starts_with('r'));
+        }
+    }
+
 }
