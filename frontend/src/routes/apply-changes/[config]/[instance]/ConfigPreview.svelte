916b03e6f7b3309adfd91720179e81084843a5c8
Ross <rosssullivan101@gmail.com>
2024-03-16 17:59:54 +0900

    Added better apply config page (#324)

--- frontend/src/routes/apply-changes/[config]/[instance]/ConfigPreview.svelte
+++ frontend/src/routes/apply-changes/[config]/[instance]/ConfigPreview.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
+    import ContentTypePill from "$lib/components/ContentTypePill.svelte";
+    import MonacoEditor from "$lib/components/MonacoEditor.svelte";
+    import { contentTypeToMonacoLanguage } from "$lib/utils/content-type-utils";
+
     export let title: string;
     export let contentType: string;
     export let data: string;
+
+    $: editorLanguage = contentTypeToMonacoLanguage(contentType);
 </script>
 
 <h2 class="text-xl font-bold mb-4">{title}</h2>
 <div>
     <p class="text-gray-600 font-semibold mb-1">Content Type</p>
-
-    <code
-        class="block bg-gray-100 rounded-md p-2 text-gray-800 whitespace-pre-wrap"
-    >
-        {contentType}
-    </code>
+    <ContentTypePill {contentType} />
 </div>
 <div class="mt-4">
     <p class="text-gray-600 font-semibold mb-1">Text</p>
-    <code
-        class="block bg-gray-100 rounded-md p-2 text-gray-800 whitespace-pre-wrap h-52 overflow-y-auto"
-    >
-        {data}
-    </code>
+
+    <div class="h-64">
+        <MonacoEditor content={data} language={editorLanguage} disabled={true} />
+    </div>
 </div>
