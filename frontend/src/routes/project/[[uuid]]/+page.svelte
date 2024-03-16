80731718acf2edf9afd020ba4e7789caf0bd0da4
Ross <rosssullivan101@gmail.com>
2024-03-16 23:56:26 +0900

    Added update project page (#330)

--- frontend/src/routes/project/[[uuid]]/+page.svelte
+++ frontend/src/routes/project/[[uuid]]/+page.svelte
@@ -5,14 +5,27 @@
     import { page } from "$app/stores";
     import { trpc } from "$lib/trpc/client";
     import { goto } from "$app/navigation";
+    import type { PageData } from "./$types";
 
-    let name = "";
+    export let data: PageData;
+
+    let projectId = $page.params.uuid;
+    const isNewProject = !projectId;
+    let name = data.project?.name ?? "";
+
+    async function onSave() {
+        if (isNewProject) {
+            onCreateProject();
+        } else {
+            // TODO: Implment
+            console.error("UPDATE PROJECT NOT IMPLMENTED");
+        }
+    }
 
     async function onCreateProject() {
         try {
-            const { projectUuid } = await trpc(
-                $page
-            ).projects.createProject.mutate(name);
+            const { projectUuid } =
+                await trpc($page).projects.createProject.mutate(name);
             goto(`/?project=${projectUuid}`);
         } catch (e) {
             console.error("Error creating project", e);
@@ -22,21 +35,35 @@
 
 <div class="container mx-auto">
     <YakManCard>
-        <h1 class="text-lg font-bold mb-4">Add Project</h1>
+        <h1 class="text-lg font-bold mb-4">
+            {#if isNewProject}
+                Add Project
+            {:else}
+                Modify Project
+            {/if}
+        </h1>
         <div class="mb-3">
             <YakManInput
                 label="Name"
                 placeholder="my-project"
                 bind:value={name}
+                disabled={!isNewProject}
                 mask="kebab-case"
             />
         </div>
+    </YakManCard>
+
+    <YakManCard extraClasses="mt-2">
         <YakManButton
-            on:click={onCreateProject}
+            on:click={onSave}
             type="submit"
             disabled={!name || name.length === 0}
         >
-            Create
+            {#if isNewProject}
+                Create
+            {:else}
+                Update
+            {/if}
         </YakManButton>
     </YakManCard>
 </div>
