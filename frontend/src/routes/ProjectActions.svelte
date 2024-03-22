27ac4d6df818f8f621e2e50d253771c49783b6dd
ranger-ross <rosssullivan101@gmail.com>
2024-03-22 22:51:50 +0900

    Accessibility fixes

--- frontend/src/routes/ProjectActions.svelte
+++ frontend/src/routes/ProjectActions.svelte
@@ -13,30 +13,30 @@
             message="Create new config"
             spacing="-translate-y-[-2.2rem]"
         >
-            <div
-                class="bg-gray-100 bg-opacity-60 hover:bg-gray-200 rounded-lg p-2"
-            >
-                <a tabindex="-1" href="/add-config?project={projectUuid}">
+            <a href="/add-config?project={projectUuid}">
+                <div
+                    class="bg-gray-100 bg-opacity-60 hover:bg-gray-200 rounded-lg p-2"
+                >
                     <div class="w-4 h-4 fill-gray-800 hover:fill-black">
                         <PlusIcon />
                     </div>
-                </a>
-            </div>
+                </div>
+            </a>
         </YakManToolTip>
 
         <YakManToolTip
             message="Project Settings"
             spacing="-translate-y-[-2.2rem]"
         >
-            <div
-                class="bg-gray-100 bg-opacity-60 hover:bg-gray-200 rounded-lg p-2"
-            >
-                <a tabindex="-1" href="/project/{projectUuid}">
+            <a href="/project/{projectUuid}">
+                <div
+                    class="bg-gray-100 bg-opacity-60 hover:bg-gray-200 rounded-lg p-2"
+                >
                     <div class="w-4 h-4 fill-gray-800 hover:fill-black">
                         <GearIcon />
                     </div>
-                </a>
-            </div>
+                </div>
+            </a>
         </YakManToolTip>
     </div>
 </YakManCard>
