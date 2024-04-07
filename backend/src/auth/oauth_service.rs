6ce9cf48ec4a3177a61f3aa4eb289a1fe8765447
ranger-ross <rosssullivan101@gmail.com>
2024-04-07 19:09:41 +0900

    more clippy fixes

--- backend/src/auth/oauth_service.rs
+++ backend/src/auth/oauth_service.rs
@@ -273,5 +273,5 @@ fn get_client_secret() -> Result<ClientSecret> {
 
 fn get_oauth_scopes() -> Vec<String> {
     let scopes = env::var("YAKMAN_OAUTH_SCOPES").expect("$YAKMAN_OAUTH_SCOPES is not set");
-    return scopes.split(",").map(|s| s.to_string()).collect();
+    return scopes.split(',').map(|s| s.to_string()).collect();
 }
